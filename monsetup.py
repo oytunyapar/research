d2668df1c3e4583da81b7d4bdaec12b54b9e8e19
Oytun Yapar <oytun.yapar@ozu.edu.tr>
2019-12-19 23:49:47 +0300

    Convert monsetup.m to python files (#1) -monsetup.py is implemented.

--- monsetup.py
+++ monsetup.py
@@ -1,2 +1,30 @@
+import numpy
+import math
+
 def monsetup(dimension):
-    return dimension
+    two_to_the_power_dimension = 2**dimension
+    D_Matrix = numpy.zeros( ( two_to_the_power_dimension, two_to_the_power_dimension ), dtype=numpy.int8 )
+    for power_of_dimension_iterator in range( 0, two_to_the_power_dimension ):
+        local_power_of_dimension_iterator = power_of_dimension_iterator
+
+        sign_vector = numpy.zeros( dimension, dtype=numpy.int8 )
+        for dimension_iterator in range( 0, dimension ):
+            if ( local_power_of_dimension_iterator % 2 == 0 ):
+                sign_vector[dimension_iterator] = 1
+            else:
+                sign_vector[dimension_iterator] = -1
+            local_power_of_dimension_iterator = math.floor(local_power_of_dimension_iterator/2)
+
+        for power_of_dimension_iterator_second in range( 0, two_to_the_power_dimension ):
+            local_power_of_dimension_iterator = power_of_dimension_iterator_second
+            multiply_factor = 1
+            for dimension_iterator in range( 0, dimension ):
+                if ( local_power_of_dimension_iterator % 2 == 1 ):
+                    multiply_factor = multiply_factor * sign_vector[dimension_iterator]
+                local_power_of_dimension_iterator = math.floor(local_power_of_dimension_iterator/2)
+            D_Matrix[ power_of_dimension_iterator, power_of_dimension_iterator_second ] = multiply_factor
+
+    print(D_Matrix)
+    return D_Matrix
+
+monsetup(4)
