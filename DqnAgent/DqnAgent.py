ea18fd7d62111c3667a33aa9a06895129f2e92b5
Oytun Yapar <yaparoytun@gmail.com>
2021-02-14 12:37:50 +0300

    DQN agent tensorflow nad pytorch agents are seperated

--- DqnAgent/DqnAgent.py
+++ DqnAgent/DqnAgent.py
@@ -1,7 +1,9 @@
 import torch
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 
 
-def create_dqn_agent(layer_sizes):
+def create_dqn_agent_torch(layer_sizes):
     model_layers = []
     for layer_iterator in range(len(layer_sizes) - 1):
         model_layers.append(torch.nn.Linear(layer_sizes[layer_iterator], layer_sizes[layer_iterator + 1]))
@@ -10,3 +12,20 @@ def create_dqn_agent(layer_sizes):
         else:
             model_layers.append(torch.nn.ReLU())
     return torch.nn.Sequential(*model_layers)
+
+
+def create_dqn_agent_tensorflow(layer_sizes):
+    model = Sequential()
+
+    for layer_iterator in range(len(layer_sizes) - 1):
+        if layer_iterator == len(layer_sizes) - 2:
+            model.add(Dense(layer_sizes[layer_iterator + 1], activation='softmax',
+                            input_dim=layer_sizes[layer_iterator],
+                            name=str(layer_iterator),))
+            break
+
+        model.add(Dense(layer_sizes[layer_iterator + 1], activation='relu', input_dim=layer_sizes[layer_iterator],
+                        name=str(layer_iterator)))
+
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
--- DqnAgent/DqnAgentTraining.py
+++ DqnAgent/DqnAgentTraining.py
@@ -5,14 +5,14 @@ current_directory = os.path.dirname(os.path.realpath(__file__))
 parent_directory = os.path.dirname(current_directory)
 sys.path.append(parent_directory)
 
-from DqnAgent.DqnAgent import create_dqn_agent
+from DqnAgent.DqnAgent import create_dqn_agent_torch, create_dqn_agent_tensorflow
 import numpy
 import monsetup
 import torch
+import tensorflow
 
 
 class DqnAgentTraining:
-
     def __init__(self, function, dimension, n_total_rl_steps,
                  n_epoch_rl_steps, batch_size, model_layer_sizes):
         self.function = function
@@ -22,25 +22,27 @@ class DqnAgentTraining:
 
         model_layer_sizes.insert(0, dimension**2)
         model_layer_sizes.append(dimension**2)
-        self.dqn_agent = create_dqn_agent(model_layer_sizes)
 
-        self.memory = numpy.zeros([n_total_rl_steps, 3*self.dimension_square + 1])
-        self.current_state = numpy.sum(self.q_matrix, 1)
+        self.memory = numpy.zeros([n_total_rl_steps, 3*self.dimension_square + 1], dtype=numpy.float32)
 
         self.n_total_rl_steps = n_total_rl_steps
         self.n_epoch_rl_steps = n_epoch_rl_steps
         self.current_rl_step = 0
 
+        self.memory_index = 0
+
         self.batch_size = batch_size
-        self.k_vector = numpy.ones([dimension**2, 1])
+        self.k_vector = numpy.ones([dimension**2, 1], dtype=numpy.float32)
+
+        self.training_function = None
 
     def get_random_batch_from_memory(self, size):
-        if self.memory.shape[0] < size:
+        if self.memory_index < size:
             print("DqnAgentTraining:get_random_batch_from_memory ERROR: invalid sized: " + str(size) + " batch request.")
             return numpy.zeros(1)
 
         rng = numpy.random.default_rng()
-        numbers = rng.choice(self.memory.shape[0], size=size, replace=False)
+        numbers = rng.choice(self.memory_index, size=size, replace=False)
         return self.memory[numbers.tolist(), :]
 
     def predicted_reward(self, next_state):
@@ -53,23 +55,83 @@ class DqnAgentTraining:
         self.memory[self.current_rl_step, self.dimension_square: 2 * self.dimension_square] = next_state
         self.memory[self.current_rl_step, 2 * self.dimension_square: 3 * self.dimension_square] = action
         self.memory[self.current_rl_step, 3 * self.dimension_square] = reward
+        self.memory_index += 1
         return
 
+    def train_agent(self):
+        while self.current_rl_step < self.n_total_rl_steps:
+            self.reinforcement_learn_step(self.n_epoch_rl_steps)
+            memory_batch = self.get_random_batch_from_memory(self.batch_size)
+            self.training_function(memory_batch[:, 0: self.dimension_square],
+                                   memory_batch[:, 2 * self.dimension_square: 3 * self.dimension_square])
+
+
+class DqnAgentTrainingTensorflow(DqnAgentTraining):
+    def __init__(self, function, dimension, n_total_rl_steps,
+                 n_epoch_rl_steps, batch_size, model_layer_sizes):
+        super().__init__(function, dimension, n_total_rl_steps, n_epoch_rl_steps, batch_size, model_layer_sizes)
+        self.dqn_agent = create_dqn_agent_tensorflow(model_layer_sizes)
+        self.current_state = numpy.sum(self.q_matrix, 1).reshape([1, self.dimension_square])
+
+        self.training_function = self.train
+
     def reinforcement_learn_step(self, step_size):
-        if step_size > 0 and self.current_rl_step + step_size < self.n_total_rl_steps:
+        if step_size > 0:
+            if self.current_rl_step + step_size > self.n_total_rl_steps:
+                step_size = self.n_total_rl_steps - self.current_rl_step
             for step in range(step_size):
-                input_tensor = torch.tensor(self.current_state, dtype=torch.float64)
+                output = self.dqn_agent(self.current_state).numpy().reshape([self.dimension_square])
+
+                selected_action = output.argmax().tolist()
+                self.k_vector[selected_action] += 1
+
+                next_state = numpy.transpose(numpy.matmul(self.q_matrix, self.k_vector)). \
+                    reshape([self.dimension_square])
+
+                reward = super().predicted_reward(next_state)
+
+                super().save_memory(self.current_state.reshape([self.dimension_square]), next_state, output.tolist(),
+                                    reward)
+
+                self.current_state = next_state.reshape([1, self.dimension_square])
+
+                self.current_rl_step += 1
+        else:
+            print("RL step size problem step size: " + str(step_size) + "\n")
+
+    def train(self, given_input, desired_output):
+        self.dqn_agent.fit(given_input,
+                           desired_output,
+                           epochs=10, batch_size=self.batch_size / 4)
+        return
+
+
+class DqnAgentTrainingPytorch(DqnAgentTraining):
+    def __init__(self, function, dimension, n_total_rl_steps,
+                 n_epoch_rl_steps, batch_size, model_layer_sizes):
+        super().__init__(function, dimension, n_total_rl_steps, n_epoch_rl_steps, batch_size, model_layer_sizes)
+        self.dqn_agent = create_dqn_agent_torch(model_layer_sizes)
+        self.current_state = numpy.sum(self.q_matrix, 1)
+
+        self.training_function = self.train
+
+    def reinforcement_learn_step(self, step_size):
+        if step_size > 0:
+            if self.current_rl_step + step_size > self.n_total_rl_steps:
+                step_size = self.n_total_rl_steps - self.current_rl_step
+            for step in range(step_size):
+                input_tensor = torch.tensor(self.current_state, dtype=torch.float32)
                 output = self.dqn_agent(input_tensor.float())
 
                 selected_action = output.argmax().tolist()
                 self.k_vector[selected_action] += 1
 
-                next_state = numpy.transpose(numpy.matmul(self.q_matrix, self.k_vector)).\
+                next_state = numpy.transpose(numpy.matmul(self.q_matrix, self.k_vector)). \
                     reshape([self.dimension_square])
 
-                reward = self.predicted_reward(next_state)
+                reward = super().predicted_reward(next_state)
 
-                self.save_memory(self.current_state, next_state, output.tolist(), reward)
+                super().save_memory(self.current_state, next_state, output.tolist(), reward)
 
                 self.current_state = next_state
 
@@ -77,17 +139,15 @@ class DqnAgentTraining:
         else:
             print("RL step size problem step size: " + str(step_size) + "\n")
 
-    def train_agent(self):
-        self.dqn_agent.train()
-
+    def train(self, given_input, desired_output):
+        loss_function = torch.nn.MSELoss(reduction='sum')
         learning_rate = 1e-3
         optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate)
-
-        loss_function = torch.nn.MSELoss(reduction='sum')
+        self.dqn_agent.train()
 
         optimizer.zero_grad()
-        #loss = loss_function(output, target)
-        #loss.backward()
+        # loss = loss_function(memory_batch[:, 2 * self.dimension_square: 3 * self.dimension_square],
+        #                     target)
+        # loss.backward()
         optimizer.step()
-
         return
