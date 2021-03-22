78a2796a4e110550858abb1ca2cd61723d7c20ec
Oytun Yapar <yaparoytun@gmail.com>
2021-03-22 21:28:11 +0300

    DQN agent training, further implementation

--- DqnAgent/DqnAgent.py
+++ DqnAgent/DqnAgent.py
@@ -1,4 +1,5 @@
 import torch
+import tensorflow
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
@@ -16,16 +17,17 @@ def create_dqn_agent_torch(layer_sizes):
 
 def create_dqn_agent_tensorflow(layer_sizes):
     model = Sequential()
-
+    initializer = tensorflow.keras.initializers.GlorotUniform()
     for layer_iterator in range(len(layer_sizes) - 1):
         if layer_iterator == len(layer_sizes) - 2:
             model.add(Dense(layer_sizes[layer_iterator + 1], activation='softmax',
                             input_dim=layer_sizes[layer_iterator],
-                            name=str(layer_iterator),))
+                            name=str(layer_iterator),
+                            kernel_initializer=initializer))
             break
 
         model.add(Dense(layer_sizes[layer_iterator + 1], activation='relu', input_dim=layer_sizes[layer_iterator],
-                        name=str(layer_iterator)))
+                        name=str(layer_iterator), kernel_initializer=initializer))
 
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     return model
