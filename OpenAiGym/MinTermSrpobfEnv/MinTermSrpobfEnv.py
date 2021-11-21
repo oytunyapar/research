1b33c6ab94d823706cfcb74a941651fcfa1bbdfe
oytun <oytunyapar@hotmail.com>
2021-11-21 23:23:12 +0300

    Further implementation

--- OpenAiGym/MinTermSrpobfEnv/MinTermSrpobfEnv.py
+++ OpenAiGym/MinTermSrpobfEnv/MinTermSrpobfEnv.py
@@ -15,26 +15,50 @@ class ActionType(Enum):
     INCREASE_DECREASE = 2
 
 
+class FunctionMode(Enum):
+    SINGLE = 1
+    LIST = 2
+    RANDOM = 3
+
+
 class MinTermSrpobfEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, function, dimension, q_matrix_representation, action_type,
-                 no_action_episode_end, episodic_reward):
+    def __init__(self, function, dimension,
+                 q_matrix_representation,
+                 action_type,
+                 no_action_episode_end,
+                 episodic_reward):
         super(MinTermSrpobfEnv, self).__init__()
         self.dimension = dimension
         self.two_to_power_dimension = 2 ** dimension
-        self.function = function
+        self.total_number_of_functions = 2 ** self.two_to_power_dimension
 
-        self.function_vector = boolean_function_generator(function, dimension)
+        if isinstance(function, int):
+            if function < self.total_number_of_functions:
+                self.function_mode = FunctionMode.SINGLE
+                self.function = function % self.total_number_of_functions
+            else:
+                self.function_mode = FunctionMode.RANDOM
+                self.function = numpy.random.randint(self.total_number_of_functions)
+        elif isinstance(function, list):
+            self.function_mode = FunctionMode.LIST
+            self.function_list = (numpy.array(function) % self.total_number_of_functions).tolist()
+            self.function_list_len = len(self.function_list)
+            self.function = self.function_list[numpy.random.randint(self.function_list_len)]
+        else:
+            raise Exception("Unsupported function type")
+
+        self.function_vector = boolean_function_generator(self.function, self.dimension)
         self.d_matrix = monsetup(dimension)
-        self.q_matrix = q_matrix_generator(function, self.dimension)
+        self.q_matrix = q_matrix_generator(self.function, self.dimension)
         self.walsh_spectrum = self.q_matrix.sum(1)
         self.k_vector_size = self.two_to_power_dimension
         self.k_vector = numpy.ones(self.two_to_power_dimension)
         self.k_vector_element_max_value = 2 ** (dimension - 1)
         self.q_matrix_representation = q_matrix_representation
 
-        if q_matrix_representation:
+        if self.q_matrix_representation:
             self.function_representation_size = self.two_to_power_dimension ** 2
             self.function_representation = self.q_matrix.reshape(1, self.function_representation_size)
         else:
@@ -76,8 +100,13 @@ class MinTermSrpobfEnv(gym.Env):
         self.cumulative_reward_in_the_episode = 0
         self.cumulative_rewards_in_the_episodes = []
 
-        self.max_reward = 0
-        self.max_reward_k_vector = self.k_vector.copy()
+        if self.function_mode is FunctionMode.SINGLE:
+            self.max_reward = 0
+            self.max_reward_k_vector = self.k_vector.copy()
+        else:
+            self.function_each_episode = []
+            self.max_reward_dict = {}
+            self.max_reward_k_vector_dict = {}
 
     def step(self, action):
         self.current_step = self.current_step + 1
@@ -103,9 +132,18 @@ class MinTermSrpobfEnv(gym.Env):
         if reward > self.max_reward_in_the_episode:
             self.max_reward_in_the_episode = reward
 
-            if reward > self.max_reward:
-                self.max_reward = reward
-                self.max_reward_k_vector = self.k_vector.copy()
+            if self.function_mode is FunctionMode.SINGLE:
+                if reward > self.max_reward:
+                    self.max_reward = reward
+                    self.max_reward_k_vector = self.k_vector.copy()
+            else:
+                if self.function in self.max_reward_dict.keys():
+                    if self.max_reward_dict[self.function] > reward:
+                        self.max_reward_dict[self.function] = reward
+                        self.max_reward_k_vector_dict[self.function] = self.k_vector.copy()
+                else:
+                    self.max_reward_dict[self.function] = reward
+                    self.max_reward_k_vector_dict[self.function] = self.k_vector.copy()
 
         if self.episodic_reward:
             if done:
@@ -183,7 +221,26 @@ class MinTermSrpobfEnv(gym.Env):
         if not self.episodic_reward:
             self.cumulative_rewards_in_the_episodes.append(self.cumulative_reward_in_the_episode)
             self.cumulative_reward_in_the_episode = 0
+
         self.k_vector = numpy.ones(self.two_to_power_dimension)
+
+        if self.function_mode is not FunctionMode.SINGLE:
+            self.function_each_episode.append(self.function)
+
+            if self.function_mode is FunctionMode.RANDOM:
+                self.function = numpy.random.randint(self.two_to_power_dimension)
+            elif self.function_mode is FunctionMode.LIST:
+                self.function = self.function_list[numpy.random.randint(self.function_list_len)]
+
+            self.function_vector = boolean_function_generator(self.function, self.dimension)
+            self.q_matrix = q_matrix_generator(self.function, self.dimension)
+            self.walsh_spectrum = self.q_matrix.sum(1)
+
+            if self.q_matrix_representation:
+                self.function_representation = self.q_matrix.reshape(1, self.function_representation_size)
+            else:
+                self.function_representation = self.walsh_spectrum
+
         observation = self.create_observation()
         return observation
 
