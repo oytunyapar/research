f263743ae2fdfce516c5e16740ae68d5663a84cc
oytun <oytunyapar@hotmail.com>
2022-02-22 22:21:49 +0300

    Further implementation

--- SigmaPiFrameworkPython/bent_functions.py
+++ SigmaPiFrameworkPython/bent_functions.py
@@ -7,7 +7,7 @@ def all_same(items):
     return all(x == items[0] for x in items)
 
 
-def find_bent_functions(dimension):
+def find_bent_functions(dimension, requested_number_of_functions=None):
     if (dimension % 2) != 0:
         print("There is not any bent function in odd dimensions.\n")
         return
@@ -17,10 +17,13 @@ def find_bent_functions(dimension):
 
     bent_function_list = []
 
-    for function_iterator in range(number_of_functions+1):
+    for function_iterator in range(number_of_functions):
         spectrum = abs(np.sum(q_matrix_generator(function_iterator, dimension), 1))
         if all_same(spectrum) and spectrum[0] > 0:
             bent_function_list.append(function_iterator)
+
+        if requested_number_of_functions is not None and requested_number_of_functions == len(bent_function_list):
+            break
 
     return np.array(bent_function_list)
 
