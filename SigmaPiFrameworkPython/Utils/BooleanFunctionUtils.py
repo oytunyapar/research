4a57ab10c812c2e3f0889b7b31713783ac026f57
oytun <oytunyapar@hotmail.com>
2022-03-26 21:46:08 +0300

    Further implementation

--- SigmaPiFrameworkPython/Utils/BooleanFunctionUtils.py
+++ SigmaPiFrameworkPython/Utils/BooleanFunctionUtils.py
@@ -39,6 +39,48 @@ def all_equivalence_classes_hex_string(dimension):
     return [hex(equivalence_class) for equivalence_class in BooleanFunctionsEquivalentClasses[dimension]]
 
 
+def get_equivalence_class_samples(dimension, sample_size, equivalence_class=None):
+    number_of_functions = 2 ** (2 ** dimension)
+    min_sample_size = 1
+    max_sample_size = int(number_of_functions / len(BooleanFunctionsEquivalentClasses[dimension]))
+
+    if min_sample_size > sample_size or sample_size > max_sample_size:
+        print("Invalid sample size:" + str(sample_size) +
+              " Min:" + str(min_sample_size) + " Max:" + str(max_sample_size))
+        return None
+
+    if equivalence_class is not None:
+        equivalence_class_spectrum = walsh_spectrum_compact(equivalence_class, dimension)
+        sampled_functions = set()
+
+        for function in range(number_of_functions):
+            if equivalence_class_spectrum == walsh_spectrum_compact(function, dimension):
+                sampled_functions.add(function)
+
+                if len(sampled_functions) == sample_size:
+                    break
+    else:
+        sampled_functions = {}
+        finished_map = {}
+        equivalence_class_strings = all_equivalence_classes_hex_string(dimension)
+
+        for equivalence_class_string in equivalence_class_strings:
+            sampled_functions[equivalence_class_string] = set()
+            finished_map[equivalence_class_string] = False
+
+        for function in range(number_of_functions):
+            equivalence_class_string = function_to_equivalence_class_hex_string(function, dimension)
+            if len(sampled_functions[equivalence_class_string]) < sample_size:
+                sampled_functions[equivalence_class_string].add(function)
+            else:
+                finished_map[equivalence_class_string] = True
+
+            if all(value for value in finished_map.values()):
+                break
+
+    return sampled_functions
+
+
 def get_functions_from_walsh_spectrum(equivalence_class, dimension):
     number_of_functions = 2**(2**dimension)
 
