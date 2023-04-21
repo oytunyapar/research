599b9f05ceb6fdbc9d49ceac0d1590100b61c087
oytun <oytunyapar@hotmail.com>
2023-04-22 00:12:14 +0300

    Further implementation

--- Utils/PlotData.py
+++ Utils/PlotData.py
@@ -2,14 +2,10 @@ from pathlib import Path
 from matplotlib import pyplot as plt
 
 
-def plot_2d(y_data, x_data=None, y_data_std=None, title="graph", graph_labels=None,
-            x_label="x", y_label="y", output_directory=None, file_name_prefix=None, show=False):
+def plot_2d(y_data, x_data=None, y_data_std=None, title="graph", x_label="x", y_label="y",
+            graph_labels=None, output_directory=None, file_name_prefix=None, show=False):
     try:
-        plt.clf()
-
-        plt.title(title, fontweight='bold')
-        plt.xlabel(x_label, labelpad=8, fontweight='bold')
-        plt.ylabel(y_label, labelpad=8, fontweight='bold')
+        init_graph(plt, title, x_label, y_label)
 
         if x_data is None:
             x_data = []
@@ -38,16 +34,71 @@ def plot_2d(y_data, x_data=None, y_data_std=None, title="graph", graph_labels=No
             plt.fill_between(x, y_minus, y_plus, alpha=.3)
 
         if no_legend is False:
-            plt.legend(loc='best')
-
-        if output_directory and file_name_prefix:
-            if not Path(output_directory).is_dir():
-                Path(output_directory).mkdir(parents=True)
-            plt.savefig(output_directory + "/" + file_name_prefix + ".png")
+            enable_legend(plt)
 
         if show:
             plt.show()
 
+        dump_png(plt, output_directory, file_name_prefix)
+
         return plt
-    except:
-        print("Plot error")
+    except Exception as e:
+        print("plot_2d error:", e)
+
+
+def plot_bar(y_data, graph_labels, graph_group_names, y_data_std=None, title="graph", x_label="x", y_label="y",
+             output_directory=None, file_name_prefix=None, show=False):
+    try:
+        init_graph(plt, title, x_label, y_label)
+
+        data_number = len(y_data)
+
+        step = 1
+        data_points = len(y_data[0])
+        space_factor = 2
+        bar_width = step/(data_number + space_factor)
+
+        x_data = [list(range(step, step * data_points + 1, step))]
+
+        for counter in range(0, data_number - 1):
+            x_data.append([x + bar_width for x in x_data[counter]])
+
+        if y_data_std is None:
+            y_data_std = []
+            for counter in range(data_number):
+                y_data_std.append(None)
+
+        for x, y, y_err, graph_label in zip(x_data, y_data, y_data_std, graph_labels):
+            plt.bar(x, y, width=bar_width, label=graph_label)
+            if y_err:
+                plt.errorbar(x, y, fmt="k_", yerr=y_err, ecolor="black", elinewidth=bar_width*5, barsabove=True)
+
+        enable_legend(plt)
+
+        plt.xticks(x_data[round(data_number/2 + 0.01) - 1], graph_group_names)
+
+        if show:
+            plt.show()
+
+        dump_png(plt, output_directory, file_name_prefix)
+    except Exception as e:
+        print("plot_bar error:", e)
+
+
+def enable_legend(plot):
+    plot.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize="small")
+    plot.tight_layout()
+
+
+def init_graph(plot, title="graph", x_label="x", y_label="y"):
+    plot.clf()
+    plot.title(title, fontweight='bold')
+    plot.xlabel(x_label, labelpad=8, fontweight='bold')
+    plot.ylabel(y_label, labelpad=8, fontweight='bold')
+
+
+def dump_png(plot, output_directory, file_name_prefix):
+    if output_directory and file_name_prefix:
+        if not Path(output_directory).is_dir():
+            Path(output_directory).mkdir(parents=True)
+        plot.savefig(output_directory + "/" + file_name_prefix + ".png")
